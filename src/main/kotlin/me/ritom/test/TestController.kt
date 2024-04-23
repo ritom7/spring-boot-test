263477b726d3dbb09f307f08f78d17367ada234a
ritom7 <ritomghosh451@gmail.com>
2024-04-23 22:02:50 +0530

    rest controllers testing

--- src/main/kotlin/me/ritom/test/TestController.kt
+++ src/main/kotlin/me/ritom/test/TestController.kt
@@ -1,7 +1,10 @@
 package me.ritom.test
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
+import java.io.File
 
 
 @RestController
@@ -10,5 +13,13 @@ class TestController {
     @GetMapping("/get")
     fun getMapping():String {
         return "This is a error"
+    }
+    @PostMapping("/post")
+    fun postMapping(@RequestBody body:String):String {
+        val file = File("someFile.txt")
+        if (!file.exists()) file.createNewFile()
+        val text = file.readText()
+        file.writeText("$text \n $body")
+        return "You said \"$body\""
     }
 }
