ce6608f3256e4344686b4d74de3c03874fdbe9ff
ritom7 <ritomghosh451@gmail.com>
2024-04-24 10:59:41 +0530

    uploading files

--- src/main/kotlin/me/ritom/test/TestController.kt
+++ src/main/kotlin/me/ritom/test/TestController.kt
@@ -1,10 +1,12 @@
 package me.ritom.test
 
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.http.MediaType
+import org.springframework.web.bind.annotation.*
+import org.springframework.web.multipart.MultipartFile
 import java.io.File
+import java.io.FileOutputStream
+
+
 
 
 @RestController
@@ -21,5 +23,14 @@ class TestController {
         val text = file.readText()
         file.writeText("$text \n $body")
         return "You said \"$body\""
+    }
+    @PostMapping("/upload",consumes = [MediaType.MULTIPART_FORM_DATA_VALUE])
+    fun postUpload(@RequestPart mFile:MultipartFile, @RequestPart(required = false) title:String?):String {
+        val file = File("files/${title}.${mFile.originalFilename?.split(".")?.get(1)}")
+        if (!file.exists()) file.createNewFile()
+        val output = FileOutputStream(file)
+        output.write(mFile.bytes)
+        output.close()
+        return "Data received"
     }
 }
