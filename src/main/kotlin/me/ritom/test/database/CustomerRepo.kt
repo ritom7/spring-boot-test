0faa2b4bdcdbe13766deabeab28513a4c72ca87a
ritom7 <ritomghosh451@gmail.com>
2024-04-26 20:32:20 +0530

    MongoDB

--- src/main/kotlin/me/ritom/test/database/CustomerRepo.kt
+++ src/main/kotlin/me/ritom/test/database/CustomerRepo.kt
@@ -3,4 +3,5 @@ package me.ritom.test.database
 import org.springframework.data.mongodb.repository.MongoRepository
 
 interface CustomerRepo : MongoRepository<Customer, Long> {
+    fun findByUsername(username: String): Customer
 }
--- src/main/kotlin/me/ritom/test/database/DatabaseController.kt
+++ src/main/kotlin/me/ritom/test/database/DatabaseController.kt
@@ -3,6 +3,7 @@ package me.ritom.test.database
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
@@ -13,10 +14,23 @@ class DatabaseController {
     private val logger: Logger = LoggerFactory.getLogger(DatabaseController::class.java)
     @Autowired
     private lateinit var repo:CustomerRepo
-    @PostMapping("/savecust")
+    @PostMapping("/customer/save")
     fun saveCustomer(@RequestBody cust:Customer):String {
         logger.info("Saving customer ${cust.username}")
         repo.save(cust)
         return "Success"
+    }
+    @PostMapping("customer/saveAll")
+    fun saveAllCustomers(@RequestBody s:String):String {
+        logger.info(s)
+        return "No customers found"
+    }
+    @GetMapping("/customer/get")
+    fun getCustomer(@RequestBody username:String):Customer {
+        return repo.findByUsername(username)
+    }
+    @GetMapping("/customer/getAll")
+    fun getAllCustomers(): List<Customer> {
+        return repo.findAll()
     }
 }
